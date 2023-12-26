f050fbc6ef8e7566565295c2fd37ea8096207988
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-12-27 00:13:47 +0100

    Refactor next.config.mjs and reducers.ts

--- src/config/reducers.ts
+++ src/config/reducers.ts
@@ -1,16 +1,18 @@
-import { combineReducers } from "redux"
-import { AnyAction } from "@reduxjs/toolkit"
-import { authSlice } from "@/modules/auth/auth.slice"
-import { AuthCallTypes } from "@/modules/auth/auth.call-types"
+import { combineReducers } from 'redux'
+import { AnyAction } from '@reduxjs/toolkit'
+import { authSlice } from '@/modules/auth/auth.slice'
+import { AuthCallTypes } from '@/modules/auth/auth.call-types'
+import darkModeReducer from '../stores/darkModeSlice'
 
 export const appReducers = combineReducers({
-	[authSlice.name]: authSlice.reducer,
+  [authSlice.name]: authSlice.reducer,
+  darkMode: darkModeReducer,
 })
 
 export const rootReducer = (state: any, action: AnyAction) => {
-	if (action?.payload?.callType === AuthCallTypes.SIGN_OUT) {
-		return appReducers(undefined, action)
-	}
+  if (action?.payload?.callType === AuthCallTypes.SIGN_OUT) {
+    return appReducers({ auth: undefined, ...state }, action)
+  }
 
-	return appReducers(state, action)
+  return appReducers(state, action)
 }
