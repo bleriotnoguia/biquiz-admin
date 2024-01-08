7452fd94585364655714f00ba33524b3214bcefd
Bleriot Noguia <nstevebleriot@yahoo.fr>
2024-01-08 23:33:00 +0100

    Update QuestionFormStepOne component

--- src/modules/admin/components/Table/QuestionFormStepOne.tsx
+++ src/modules/admin/components/Table/QuestionFormStepOne.tsx
@@ -7,27 +7,27 @@ import { mdiMail } from '@mdi/js'
 const QuestionFormStepOne = () => {
   return (
     <>
-      <FormField label="Name" icons={[mdiMail, mdiMail]}>
+      <FormField label="Name - en | fr" icons={[mdiMail, mdiMail]}>
         <Field name="name_en" placeholder="Name EN" />
         <Field name="name_fr" placeholder="Name FR" />
       </FormField>
 
       <FormField label="Category" labelFor="category" help="Select one category">
-        <Field name="category" id="category" component="select">
+        <Field name="category_id" id="category" component="select">
           <option value="1">Homme / Man</option>
           <option value="2">Femme / Women</option>
           <option value="3">Lieux / places</option>
         </Field>
       </FormField>
 
-      <FormField label="Source Text" icons={[mdiMail, mdiMail]}>
+      <FormField label="Source Text - en | fr" icons={[mdiMail, mdiMail]}>
         <Field name="source_text_en" placeholder="Source Text EN" />
         <Field name="source_text_fr" placeholder="Source Text FR" />
       </FormField>
 
       <FormField label="Is Active">
         <FormCheckRadio type="switch">
-          <Field type="checkbox" name="is_active" value="ipsum" />
+          <Field type="checkbox" name="is_active" />
         </FormCheckRadio>
       </FormField>
     </>
