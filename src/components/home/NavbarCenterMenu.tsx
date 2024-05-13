95dee91445369001ab61a47738a8b16bd32ef3f6
Bleriot Noguia <nstevebleriot@yahoo.fr>
2024-05-13 15:57:27 +0100

    refactor: Update NavbarCenterMenu component structure and add mobile menu functionality

--- src/components/home/NavbarCenterMenu.tsx
+++ src/components/home/NavbarCenterMenu.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Disclosure } from '@headlessui/react'
 import LogoImg from '../../../public/img/dummylogo.svg'
 import AuthButton from '../AuthButton'
 
@@ -11,14 +12,55 @@ export default function NavbarCenterMenu() {
     <>
       <div className="w-full ">
         <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
-          <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
-            <Link href="#" className="mr-2">
-              <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-                <Image src={LogoImg} alt="logo" />
-              </span>
-            </Link>
+          {/* Logo  */}
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
+                  <Link href="#">
+                    <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
+                      <Image src={LogoImg} alt="logo" />
+                    </span>
+                  </Link>
 
-            <AuthButton />
+                  <Disclosure.Button
+                    aria-label="Toggle Menu"
+                    className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-gray-700 dark:focus:text-gray-300"
+                  >
+                    <svg
+                      className="w-6 h-6 fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      {open && (
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                        />
+                      )}
+                      {!open && (
+                        <path
+                          fillRule="evenodd"
+                          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                        />
+                      )}
+                    </svg>
+                  </Disclosure.Button>
+
+                  <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
+                    <AuthButton />
+                  </Disclosure.Panel>
+                </div>
+              </>
+            )}
+          </Disclosure>
+
+          {/* menu  */}
+          <div className="hidden text-center lg:flex lg:items-center">
+            <ul className="items-center justify-end flex-1 pt-6 lg:pt-0 list-reset lg:flex">
+              <AuthButton />
+            </ul>
           </div>
         </nav>
       </div>
