4047a355865ad6dba5cc1bb99b37f4c5cd640cda
Bleriot Noguia <nstevebleriot@yahoo.fr>
2024-05-30 19:41:08 +0100

    refactor: Update package.json dependencies and remove unused code

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,41 +4,16 @@ import {
   mdiAccessPoint,
   mdiAccountMultiple,
   mdiCartOutline,
-  mdiChartPie,
   mdiChartTimelineVariant,
-  mdiMonitorCellphone,
-  mdiReload,
 } from '@mdi/js'
-import React, { useState } from 'react'
+import React from 'react'
 import Button from '@/modules/admin/components/Button'
 import SectionMain from '@/modules/admin/components/Section/Main'
 import SectionTitleLineWithButton from '@/modules/admin/components/Section/TitleLineWithButton'
 import CardBoxWidget from '@/modules/admin/components/CardBox/Widget'
-import { useSampleClients, useSampleTransactions } from '@/modules/admin/hooks/sampleData'
-import CardBoxTransaction from '@/modules/admin/components/CardBox/Transaction'
-import { Client, Transaction } from '@/modules/admin/interfaces'
-import CardBoxClient from '@/modules/admin/components/CardBox/Client'
 import SectionBannerStarOnGitHub from '@/modules/admin/components/Section/Banner/StarOnGitHub'
-import CardBox from '@/modules/admin/components/CardBox'
-import { sampleChartData } from '@/modules/admin/components/ChartLineSample/config'
-import ChartLineSample from '@/modules/admin/components/ChartLineSample'
-import NotificationBar from '@/modules/admin/components/NotificationBar'
-import TableSampleClients from '@/modules/admin/components/Table/SampleClients'
 
 const DashboardPage = () => {
-  const { clients } = useSampleClients()
-  const { transactions } = useSampleTransactions()
-
-  const clientsListed = clients.slice(0, 4)
-
-  const [chartData, setChartData] = useState(sampleChartData())
-
-  const fillChartData = (e: React.MouseEvent) => {
-    e.preventDefault()
-
-    setChartData(sampleChartData())
-  }
-
   return (
     <SectionMain>
       <SectionTitleLineWithButton icon={mdiChartTimelineVariant} title="Overview" main>
@@ -85,38 +60,9 @@ const DashboardPage = () => {
         />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
-        <div className="flex flex-col justify-between">
-          {transactions.map((transaction: Transaction) => (
-            <CardBoxTransaction key={transaction.id} transaction={transaction} />
-          ))}
-        </div>
-        <div className="flex flex-col justify-between">
-          {clientsListed.map((client: Client) => (
-            <CardBoxClient key={client.id} client={client} />
-          ))}
-        </div>
-      </div>
-
       <div className="my-6">
         <SectionBannerStarOnGitHub />
       </div>
-
-      <SectionTitleLineWithButton icon={mdiChartPie} title="Trends overview">
-        <Button icon={mdiReload} color="whiteDark" onClick={fillChartData} />
-      </SectionTitleLineWithButton>
-
-      <CardBox className="mb-6">{chartData && <ChartLineSample data={chartData} />}</CardBox>
-
-      <SectionTitleLineWithButton icon={mdiAccountMultiple} title="Clients" />
-
-      <NotificationBar color="info" icon={mdiMonitorCellphone}>
-        <b>Responsive table.</b> Collapses on mobile
-      </NotificationBar>
-
-      <CardBox hasTable>
-        <TableSampleClients />
-      </CardBox>
     </SectionMain>
   )
 }
